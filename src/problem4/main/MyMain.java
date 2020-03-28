975ffa3e19486bf80dd49c660955e6c06b566c07
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2020-03-28 15:50:01 +0530

    final commit

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,5 +7,25 @@
 package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem4.binaryST.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+// executable class
+// use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(myQueue);
+        myBinarySearchTree.insert(67);
+        myBinarySearchTree.insert(20);
+        myBinarySearchTree.insert(31);
+        myBinarySearchTree.insert(40);
+        myBinarySearchTree.insert(10);
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder successors of all the nodes");
+        myQueue.show();
+
+    }
+
+
 }
