a300af50b75d77bfed621c804ecd05d3a68caa65
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2020-03-28 15:21:41 +0530

    fnal commit

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,22 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
-// executable class
-// use problem5.student.Student class to create object of student
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student("saur", 58));
+        queue.enqueue(new Student("raje", 23));
+        queue.enqueue(new Student("Pavitra", 65));
+        queue.enqueue(new Student("lamar", 1));
+        queue.enqueue(new Student("mita", 10));
+        queue.enqueue(new Student("Shantanu", 76));
+
+        queue.show();
+        queue.dequeue();
+        queue.show();
+
+    }
 }
